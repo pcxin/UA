d12385680c284783cbc0dd0f51c6736778144551
黄珂 <huangkelucky7@qq.com>
2015-08-06 12:11:51 +0800

    增加了小米的支付方法

--- src/com/yychina/channel/allsdk/Sdk_Mi.java
+++ src/com/yychina/channel/allsdk/Sdk_Mi.java
@@ -7,17 +7,21 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.unity3d.player.UnityPlayer;
+import com.xiaomi.gamecenter.sdk.GameInfoField;
 import com.xiaomi.gamecenter.sdk.MiCommplatform;
 import com.xiaomi.gamecenter.sdk.MiErrorCode;
 import com.xiaomi.gamecenter.sdk.OnLoginProcessListener;
+import com.xiaomi.gamecenter.sdk.OnPayProcessListener;
 import com.xiaomi.gamecenter.sdk.entry.MiAccountInfo;
 import com.xiaomi.gamecenter.sdk.entry.MiAppInfo;
+import com.xiaomi.gamecenter.sdk.entry.MiBuyInfo;
 import com.yychina.channel.SdkBaseFactory;
 import com.yychina.channel.SdkPayInfo;
 
@@ -35,7 +39,7 @@ public class Sdk_Mi implements SdkBaseFactory {
 		tag = Sdk_Mi.class.getName();
 		context = c;
 		if (context == null) {
-			Log.e(tag, "sdsddddddddddddddddddddddsd");
+			Log.e("HuangKe----->", "传入的context为空");
 		}
 		// TODO Auto-generated method stub
 		/** SDK初始化 */
@@ -75,7 +79,7 @@ public class Sdk_Mi implements SdkBaseFactory {
 								dataJsonObj.put("user_id", userId);
 								dataJsonObj.put("token", token);
 							} catch (Exception e) {
-
+								e.printStackTrace();
 							}
 
 							handler.sendEmptyMessage(30000);
@@ -88,6 +92,7 @@ public class Sdk_Mi implements SdkBaseFactory {
 								dataJsonObj.put("token", "");
 							} catch (Exception e) {
 								// TODO: handle exception
+								e.printStackTrace();
 							}
 							handler.sendEmptyMessage(40000);
 						}
@@ -99,6 +104,7 @@ public class Sdk_Mi implements SdkBaseFactory {
 								dataJsonObj.put("token", "");
 								}catch (Exception e) {
 									// TODO: handle exception
+									e.printStackTrace();
 								}
 						
 							handler.sendEmptyMessage(70000);
@@ -152,15 +158,16 @@ public class Sdk_Mi implements SdkBaseFactory {
 
 	@Override
 	public void pay(String jsonData) {
+		final JSONObject dataJsonObj = new JSONObject();
 		try {
 			JSONObject json = new JSONObject(jsonData);
 			json.put("order_id", UUID.randomUUID().toString());
-			json.put("token", accountInfo.getSessionId());
-			json.put("money", "0.01");
+			json.put("token", token);
+			json.put("money", "5");
 			json.put("rmb", "0.01");
 			json.put("rate", "0.01");
 			json.put("productName", "充值1");
-			json.put("count", "1");
+			json.put("count", "3");
 			json.put("productId", "cc1");
 			json.put("notify_uri", "http://www.ddd.com");
 			json.put("roleName", "小诗子");
@@ -168,12 +175,83 @@ public class Sdk_Mi implements SdkBaseFactory {
 			json.put("roleLevel", "12");
 			json.put("roleVIPLevel", "1");
 			json.put("rolePartyName", "说的");
-			json.put("channelId", accountInfo.getUid());
+			json.put("channelId", "xiaomi");
 			json.put("serverId", "sdssdss237283sdj");
-			json.put("serverName", "都是");
+			json.put("serverName", "战斗服");
 			json.put("app_ext", "dsds");
 			
-			SdkPayInfo i = new SdkPayInfo().converInfo(json.toString());
+			final SdkPayInfo sdkPayinfo = new SdkPayInfo().converInfo(json.toString());
+			
+			
+			MiBuyInfo miBuyInfo= new MiBuyInfo();
+			miBuyInfo.setCpOrderId(UUID.randomUUID().toString());//订单号唯一（不为空）
+			miBuyInfo.setCpUserInfo("cpUserInfo"); //此参数在用户支付成功后会透传给CP的服务器
+			final int amount = Integer.parseInt(sdkPayinfo.count) * Integer.parseInt(sdkPayinfo.money);
+			miBuyInfo.setAmount( amount ); //必须是大于1的整数，10代表10米币，即10元人民币（不为空）
+			//用户信息，网游必须设置、单机游戏或应用可选**
+			
+			Bundle mBundle = new Bundle();
+			mBundle.putString( GameInfoField.GAME_USER_BALANCE, sdkPayinfo.userBalance );   //用户余额
+			mBundle.putString( GameInfoField.GAME_USER_GAMER_VIP, sdkPayinfo.roleVIPLevel );  //vip等级
+			mBundle.putString( GameInfoField.GAME_USER_LV, sdkPayinfo.roleLevel );  //角色等级
+			mBundle.putString( GameInfoField.GAME_USER_PARTY_NAME, sdkPayinfo.rolePartyName);  //工会，帮派
+			mBundle.putString( GameInfoField.GAME_USER_ROLE_NAME, sdkPayinfo.roleName ); //角色名称
+			mBundle.putString( GameInfoField.GAME_USER_ROLEID, sdkPayinfo.roleId);    //角色id
+			mBundle.putString( GameInfoField.GAME_USER_SERVER_NAME, sdkPayinfo.serverName );  //所在服务器
+			miBuyInfo.setExtraInfo( mBundle ); //设置用户信息
+			MiCommplatform.getInstance().miUniPay((Activity)context, miBuyInfo, 
+			new OnPayProcessListener()
+			{
+			@Override
+			    public void finishPayProcess( int code ) {
+			        switch( code ) {
+			        case MiErrorCode.MI_XIAOMI_PAYMENT_SUCCESS:
+			       //购买成功
+			        	Log.i("HuangKe----->", "支付成功");
+			        	Log.i("HuangKe----->", "金额是:"+amount);
+			        	try {
+							dataJsonObj.put("status", "success");
+							dataJsonObj.put("user_id", userId);
+							dataJsonObj.put("productName", sdkPayinfo.productName);
+							dataJsonObj.put("totalmoney", amount);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+			            break;
+			        case MiErrorCode.MI_XIAOMI_PAYMENT_ERROR_PAY_CANCEL:
+			            //取消购买
+			        	Log.i("HuangKe----->", "取消购买");
+			        	try {
+							dataJsonObj.put("status", "cancel");
+							dataJsonObj.put("user_id", userId);
+							dataJsonObj.put("productName", "");
+							dataJsonObj.put("totalmoney", "");
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+			               break;
+			        case MiErrorCode.MI_XIAOMI_PAYMENT_ERROR_PAY_FAILURE:
+			            //购买失败
+			        	Log.i("HuangKe----->", "购买失败");
+			        	try {
+							dataJsonObj.put("status", "failed");
+							dataJsonObj.put("user_id", userId);
+							dataJsonObj.put("productName", "");
+							dataJsonObj.put("totalmoney", "");
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+			               break;       
+			case  MiErrorCode.MI_XIAOMI_PAYMENT_ERROR_ACTION_EXECUTED:  
+			   //操作正在进行中
+			   break;       
+			        default:
+			              //购买失败
+			            break;
+			        }
+			    }
+			});
 			
 			
 			
@@ -183,7 +261,8 @@ public class Sdk_Mi implements SdkBaseFactory {
 		}
 		
 		// TODO Auto-generated method stub
-		UnityPlayer.UnitySendMessage("Main Camera", "messgae", "javaData");
+		String jsonResult = dataJsonObj.toString();
+		UnityPlayer.UnitySendMessage("Main Camera","messgae",jsonResult);
 	}
 
 	@Override
